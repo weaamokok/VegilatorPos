4eff8d598f69f87db57e5f7434f659e1ac68c1e2
weaamokok <47139547+weaamokok@users.noreply.github.com>
2023-10-01 23:58:29 +0200

    ..

--- lib/src/config/database_helper.dart
+++ lib/src/config/database_helper.dart
@@ -5,6 +5,7 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:vege/src/mange%20inventory/domin/models/vegetable.dart';
+import 'package:vege/src/utils/constants.dart';
 
 class DatabaseProvider {
   DatabaseProvider._();
@@ -23,17 +24,62 @@ late  Database _database;
     String path = join(directory.path, "vege.db");
     return await openDatabase(path, version: 1,
         onCreate: (Database db, int version) async {
-      await db.execute("CREATE TABLE vegetable ("
-          "id integer primary key AUTOINCREMENT,"
-          "name TEXT,"
-          "code TEXT"
-          "sale_price TEXT"
-          "buy_price TEXT"
-          "catagory_id TEXT"
-          "type TEXT"
-          "image TEXT"
-          "user_id TEXT"//foreign key
+            await db.execute("CREATE TABLE ${AppConst.position} ("
+          "${AppConst.id} integer primary key AUTOINCREMENT,"
+          "${AppConst.title} TEXT,"
+        
+          ")");    
+            await db.execute("CREATE TABLE ${AppConst.user} ("
+          "${AppConst.id} integer primary key AUTOINCREMENT,"
+          "${AppConst.firstName} TEXT,"
+          "${AppConst.lastName} TEXT"
+          "${AppConst.password} TEXT"
+          "${AppConst.phoneNumber} TEXT"
+          "${AppConst.positionId} TEXT"
+          "${AppConst.email} TEXT"
+          "${AppConst.accountState} TEXT"
+          "FOREIGN KEY(${AppConst.positionId}) REFERENCES ${AppConst.position}(${AppConst.id})"//foreign key
+
           ")");
+
+      await db.execute("CREATE TABLE ${AppConst.catagory} ("
+          "${AppConst.id} integer primary key AUTOINCREMENT,"
+          "${AppConst.catagory} TEXT,"
+
+          ")"); 
+           await db.execute("CREATE TABLE ${AppConst.vegetable} ("
+          "${AppConst.id} integer primary key AUTOINCREMENT,"
+          "${AppConst.name} TEXT,"
+          "${AppConst.code} TEXT"
+          "${AppConst.salePrice} REAL"
+          "${AppConst.buyPrice} REAL"
+          "${AppConst.catagoryId} integer"
+          "${AppConst.image} TEXT"
+          "${AppConst.userId} integer"
+          "FOREIGN KEY(${AppConst.userId}) REFERENCES ${AppConst.user}(${AppConst.id})"//foreign key
+          "FOREIGN KEY(${AppConst.catagoryId}) REFERENCES ${AppConst.catagory}(${AppConst.id})"//foreign key
+          ")");   
+          
+          await db.execute("CREATE TABLE ${AppConst.invoice} ("
+          "${AppConst.id} integer primary key AUTOINCREMENT,"
+          "${AppConst.totalAmount} REAL"
+          "${AppConst.dateRecorded} TEXT"
+          "${AppConst.paymentType} TEXT"
+          "${AppConst.userId} integer"
+          "FOREIGN KEY(${AppConst.userId}) REFERENCES ${AppConst.user}(${AppConst.id})"//foreign key
+          ")"); 
+           await db.execute("CREATE TABLE ${AppConst.sale} ("
+          "${AppConst.id} integer primary key AUTOINCREMENT,"
+          "${AppConst.subTotal} REAL"
+          "${AppConst.unitPrice} REAL"
+          "${AppConst.vegeId} integer"
+          "${AppConst.invoiceId} integer"
+          "${AppConst.quantity} integer"
+          "FOREIGN KEY(${AppConst.invoiceId}) REFERENCES ${AppConst.invoice}(${AppConst.id})"//foreign key
+          "FOREIGN KEY(${AppConst.vegeId}) REFERENCES ${AppConst.vegetable}(${AppConst.id})"//foreign key
+          ")"); 
+          
+
     });
   }
 
